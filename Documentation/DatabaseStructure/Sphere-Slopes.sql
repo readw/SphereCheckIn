d718711d122053ce3377c3b7fe5be1562344adfd
William Read <readw@users.noreply.github.com>
2017-03-06 14:33:53 +0000

    Database Content Inclusion
    Add content and updated structure for Ski-Slope database.

--- Documentation/DatabaseStructure/Sphere-Slopes.sql
+++ Documentation/DatabaseStructure/Sphere-Slopes.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.6.33)
 # Database: Sphere-Slopes
-# Generation Time: 2017-03-06 12:37:08 +0000
+# Generation Time: 2017-03-06 14:32:01 +0000
 # ************************************************************
 
 
@@ -28,18 +28,24 @@ DROP TABLE IF EXISTS `Customers`;
 CREATE TABLE `Customers` (
   `customerId` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `userId` int(6) unsigned zerofill NOT NULL,
-  `memStatus` int(11) NOT NULL,
-  `sessionNo` int(11) unsigned DEFAULT NULL,
+  `memStatus` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`customerId`),
   KEY `userId_Users` (`userId`),
   KEY `memStatus` (`memStatus`),
-  KEY `sessionNo_Session` (`sessionNo`),
   CONSTRAINT `customerId_SessionCustomer` FOREIGN KEY (`customerId`) REFERENCES `Session-Customer` (`customerId`),
   CONSTRAINT `memStatus` FOREIGN KEY (`memStatus`) REFERENCES `MembershipTypes` (`membershipId`),
-  CONSTRAINT `sessionNo_Session` FOREIGN KEY (`sessionNo`) REFERENCES `Session` (`sessionId`),
   CONSTRAINT `userId_Users` FOREIGN KEY (`userId`) REFERENCES `Users` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `Customers` WRITE;
+/*!40000 ALTER TABLE `Customers` DISABLE KEYS */;
+
+INSERT INTO `Customers` (`customerId`, `userId`, `memStatus`)
+VALUES
+	(2,000002,1);
+
+/*!40000 ALTER TABLE `Customers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Employee
@@ -52,10 +58,19 @@ CREATE TABLE `Employee` (
   `empRole` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`userId`),
   KEY `empRole_Employee` (`empRole`),
-  CONSTRAINT `empRole_Employee` FOREIGN KEY (`empRole`) REFERENCES `EmployeeRoles` (`roleId`),
-  CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `Users` (`userId`)
+  CONSTRAINT `empRole_Employee` FOREIGN KEY (`empRole`) REFERENCES `EmployeeRoles` (`roleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `Employee` WRITE;
+/*!40000 ALTER TABLE `Employee` DISABLE KEYS */;
+
+INSERT INTO `Employee` (`userId`, `empRole`)
+VALUES
+	(000000,1),
+	(000001,3);
+
+/*!40000 ALTER TABLE `Employee` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table EmployeeRoles
@@ -69,6 +84,17 @@ CREATE TABLE `EmployeeRoles` (
   PRIMARY KEY (`roleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `EmployeeRoles` WRITE;
+/*!40000 ALTER TABLE `EmployeeRoles` DISABLE KEYS */;
+
+INSERT INTO `EmployeeRoles` (`roleId`, `role`)
+VALUES
+	(1,'Instructor'),
+	(2,'Slope Operator'),
+	(3,'Admin');
+
+/*!40000 ALTER TABLE `EmployeeRoles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table MembershipTypes
@@ -82,6 +108,17 @@ CREATE TABLE `MembershipTypes` (
   PRIMARY KEY (`membershipId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `MembershipTypes` WRITE;
+/*!40000 ALTER TABLE `MembershipTypes` DISABLE KEYS */;
+
+INSERT INTO `MembershipTypes` (`membershipId`, `membership`)
+VALUES
+	(1,'None'),
+	(2,'Basic'),
+	(3,'Gold');
+
+/*!40000 ALTER TABLE `MembershipTypes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Session
@@ -102,6 +139,22 @@ CREATE TABLE `Session` (
   CONSTRAINT `type_SessionType` FOREIGN KEY (`type`) REFERENCES `SessionType` (`typeId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `Session` WRITE;
+/*!40000 ALTER TABLE `Session` DISABLE KEYS */;
+
+INSERT INTO `Session` (`sessionId`, `type`, `sessionDate`, `startTime`, `endTime`, `assignedInstructor`, `maxUsers`)
+VALUES
+	(1,1,'2017-03-07','10:00:00','11:00:00',NULL,100),
+	(2,2,'2017-03-07','11:00:00','12:00:00',NULL,50),
+	(3,3,'2017-03-08','10:00:00','11:00:00',000000,6),
+	(4,1,'2017-03-08','15:00:00','17:00:00',NULL,100),
+	(5,2,'2017-03-09','09:00:00','10:00:00',NULL,50),
+	(6,2,'2017-03-09','11:00:00','12:00:00',NULL,50),
+	(7,1,'2017-03-10','09:00:00','17:00:00',NULL,100),
+	(8,1,'2017-03-11','09:00:00','14:00:00',NULL,100);
+
+/*!40000 ALTER TABLE `Session` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Session-Customer
@@ -110,13 +163,23 @@ CREATE TABLE `Session` (
 DROP TABLE IF EXISTS `Session-Customer`;
 
 CREATE TABLE `Session-Customer` (
-  `customerId` int(11) unsigned zerofill NOT NULL,
+  `customerId` int(11) unsigned NOT NULL,
   `sessionId` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`customerId`,`sessionId`),
-  KEY `sessionId_Session` (`sessionId`),
-  CONSTRAINT `sessionId_Session` FOREIGN KEY (`sessionId`) REFERENCES `Session` (`sessionId`)
+  PRIMARY KEY (`customerId`,`sessionId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `Session-Customer` WRITE;
+/*!40000 ALTER TABLE `Session-Customer` DISABLE KEYS */;
+
+INSERT INTO `Session-Customer` (`customerId`, `sessionId`)
+VALUES
+	(2,1),
+	(2,2),
+	(2,3),
+	(2,4);
+
+/*!40000 ALTER TABLE `Session-Customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table SessionType
@@ -130,6 +193,17 @@ CREATE TABLE `SessionType` (
   PRIMARY KEY (`typeId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `SessionType` WRITE;
+/*!40000 ALTER TABLE `SessionType` DISABLE KEYS */;
+
+INSERT INTO `SessionType` (`typeId`, `type`)
+VALUES
+	(1,'Free Sessions'),
+	(2,'Membership Sessions'),
+	(3,'Instructor Sessions');
+
+/*!40000 ALTER TABLE `SessionType` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table Users
@@ -146,13 +220,24 @@ CREATE TABLE `Users` (
   `address 2` varchar(100) DEFAULT NULL,
   `address 3` varchar(100) DEFAULT NULL,
   `townCity` varchar(100) NOT NULL DEFAULT '',
-  `county` varchar(100) NOT NULL DEFAULT '',
+  `county` varchar(100) DEFAULT NULL,
   `postcode` varchar(7) NOT NULL DEFAULT '',
   `telephone` varchar(15) NOT NULL DEFAULT '',
   `email` varchar(320) NOT NULL DEFAULT '',
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+
+INSERT INTO `Users` (`userId`, `firstName`, `lastName`, `dateOfBirth`, `address 1`, `address 2`, `address 3`, `townCity`, `county`, `postcode`, `telephone`, `email`)
+VALUES
+	(000000,'Justin','Jackman','1996-12-06','42 Warner Road',NULL,NULL,'Longfield','Kent','CP15EP','07955077663','justin@jackman.com'),
+	(000001,'Hamelton','Philips','1983-04-05','1 Musket Avenue',NULL,NULL,'Wellingborough','Northamptonshire','NN297LP','07586546897','phil@ham.com'),
+	(000002,'Bill','Jobs','1995-08-15','1 Summersby Way',NULL,NULL,'London',NULL,'LN958IP','07951904675','jobs@bill.com');
+
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
