502c24e85e21661cd2d3787b7c8a3dde95668039
Lwalsh13 <Lwalsh13@users.noreply.github.com>
2017-03-20 12:20:11 +0000

    Add files via upload

---
+++ Prototype1/CheckinFunction/CheckIn/_Layout.php
@@ -0,0 +1,22 @@
+        require 
+        <div>
+            <h1> Enter details below </h1>
+        </div>
+        
+        <h2>
+        <form action="Checkin.php" method="post">
+        MemberNumber: <input type="text" name="Member number"/><br/>
+        Date of birth: <input type="Date" name="DOB"/><br/>
+        <input type="Submit" Value= "Enter">
+            
+            <?php
+                require'Server.php';
+                if(MemberNumber != 'customerId'){
+                //reroute 
+                }
+            else{
+                else("Echo");
+            }  
+            ?>
+
+    
